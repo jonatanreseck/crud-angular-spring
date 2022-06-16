820fabd7ad1780b125e493358b98dbba73080d59
Jonatan <jonatanreseck@yahoo.com.br>
2022-06-16 15:48:00 -0300

    aula 7: adicionado css na tabela de cursos

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category:'Front-End'}
+  ];
+  displayedColumns = [ 'name', 'category' ]
 
   constructor() { }
 
